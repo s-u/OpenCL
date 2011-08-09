df4f6c0353e350539fb7deaaeeb0c386b6983fc1
Simon Urbanek <simon.urbanek@R-project.org>
2011-08-09 20:48:47 +0000

    add subsetting operator to clFloat

--- R/clFloat.R
+++ R/clFloat.R
@@ -7,3 +7,5 @@ as.character.clFloat <- function(x, ...) as.character(.Call("float2double", x), 
 print.clFloat <- function(x, ...) { cat(" Object of class clFloat:\n"); print(.Call("float2double", x), ...) }
 length.clFloat <- function(x) .Call("clFloat_length", x)
 "length<-.clFloat" <- function(x, value) .Call("clFloat_length_set", x, value)
+`[.clFloat` <- function(x, ...) .Call("double2float", `[`(as.double(x), ...))
+`[<-.clFloat` <- function(x, ..., value) .Call("double2float", `[<-`(as.double(x), ..., value))
